ef38fb18cf04dcfe418d01094240f73b1af24ef4
Jonas Nilsson <jonilss@gmail.com>
2017-11-20 10:00:30 +0100

    added test project for unit tests

---
+++ Enigma/Enigma.Test/SLIPPacketTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Enigma.Model;
+
+namespace Enigma.Test
+{
+    [TestClass]
+    public class SLIPPacketTests
+    {
+        [TestMethod]
+        public void ShouldAddENDbyteToStream()
+        {
+            //Arrange
+            Parameter p = new Parameter()
+            {
+                Id = 1,
+                Value = 10
+            };
+                
+            //Act
+            var data = SLIPPacket.ToByteArray(p);
+
+            //Assert
+            Assert.AreEqual(SLIPPacket.END, data.Last());
+
+        }
+
+    }
+}
