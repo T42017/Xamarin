d2a04b2896c4dee1485a95458653469ea95c887d
Mathias Andersson <mathias.andersson@lund.ntig.se>
2017-11-21 10:33:31 +0100

    Added a splash screen that lasts for 3 seconds.

---
+++ Enigma/Enigma/Enigma.Android/SplashActivity.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Application = Android.App.Application;
+
+namespace Enigma.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        private bool _started = false;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistantState)
+        {
+            base.OnCreate(savedInstanceState, persistantState);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            if (!_started)
+            {
+                _started = true;
+                Task startupWork = new Task(() => { SimulateStartup(); });
+                startupWork.Start();
+            }
+            else
+            {
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            }
+        }
+
+        async void SimulateStartup()
+        {
+
+            await Task.Delay(3000); 
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
