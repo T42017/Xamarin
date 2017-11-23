b3b841212e192b4827d1fdd14a3de89ffea17d44
Niklas Lindblom-Hertz <niklas.lindblomhertz@lund.ntig.se>
2017-11-23 13:40:28 +0100

    Can navigate between different content pages using buttons or swiping.

--- Enigma/Enigma/Enigma/MainPage.xaml.cs
+++ Enigma/Enigma/Enigma/MainPage.xaml.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Enigma
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : CarouselPage
     {
+        private SettingsPage setting;
         public MainPage()
         {
             InitializeComponent();
+            setting = new SettingsPage();
+        }
+
+
+        public ContentPage MyProperty
+        {
+            get { return CurrentPage; }
+            set { CurrentPage = value; }
+        }
+
+
+        public void BtnPageChangeSettings(object sender, EventArgs e)
+        {
+            
+            this.CurrentPage = CurrentPage.FindByName<ContentPage>("SettingsPage");
+        }
+
+        private void BtnChangePageRotera(object sender, EventArgs e)
+        {
+            this.CurrentPage = CurrentPage.FindByName<ContentPage>("RotationPage");
+        }
+
+        private void BtnPageChangeDiagnostic(object sender, EventArgs e)
+        {
+            this.CurrentPage = CurrentPage.FindByName<ContentPage>("DiagnosticsPage");
+        }
+
+        private void BtnPageChangeMap(object sender, EventArgs e)
+        {
+            this.CurrentPage = CurrentPage.FindByName<ContentPage>("MapsPage");
         }
     }
 }
