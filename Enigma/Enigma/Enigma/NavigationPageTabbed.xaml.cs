7f9e877910fc14c5723bd1ffbc81271cd0853f8e
Niklas Lindblom-Hertz <niklas.lindblomhertz@lund.ntig.se>
2017-11-27 11:51:56 +0100

    Added framework for
    navigation

--- Enigma/Enigma/Enigma/NavigationPageTabbed.xaml.cs
+++ Enigma/Enigma/Enigma/NavigationPageTabbed.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Enigma
 {
-    public partial class MainPage : NavigationPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPageTabbed : TabbedPage
     {
-        public MainPage()
+        public NavigationPageTabbed ()
         {
             InitializeComponent();
         }
