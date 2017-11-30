3adee8fe940ab153f654b20996a888b8f9764748
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-11-30 10:56:43 +0100

    temp with nu get

--- Enigma/Enigma/Enigma/MapPage.xaml.cs
+++ Enigma/Enigma/Enigma/MapPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Maps;
+using Xamarin.Forms.GoogleMaps;
 
 namespace Enigma
 {
@@ -13,29 +13,29 @@ namespace Enigma
 		public MapPage ()
 		{
 		    InitializeComponent();
-		    var map = new Map(
-		        MapSpan.FromCenterAndRadius(
-		            new Position(55.696922, 13.195254), Distance.FromMiles(1.0)))
+		    var map = new Map()
 		    {
-		        IsShowingUser = true,
+                MyLocationEnabled = true,
+                		        
 		        HeightRequest = 320,
 		        WidthRequest = 200,
 		        VerticalOptions = LayoutOptions.CenterAndExpand
 		    };
+            
 		    var position = new Position(55.696922, 13.195254);
-		    var pin = new Pin
+		    var pin = new Pin()
 		    {
+                
 		        Type = PinType.Place,
 		        Position = position,
-		        Label = "custom pin",
+                Icon = BitmapDescriptorFactory.FromBundle("meme.png"),
+		        Label = "custom pin",               
 		        Address = "custom detail info"
 		    };
 		    map.Pins.Add(pin);
-		    //var cp = new ContentPage
-		    //{
-		    //    Content = map,
-		    //};
-
+		    map.UiSettings.CompassEnabled = true;
+		    var position1= new Position(56.172442, 14.860493);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(position1, Distance.FromKilometers(150)));
 		    this.Content = map;
         }
 	}
