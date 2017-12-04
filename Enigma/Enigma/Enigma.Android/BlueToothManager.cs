e374916676877e82c634b93af2676842ca14affc
Mathias Andersson <mathias.andersson@lund.ntig.se>
2017-12-04 11:06:42 +0100

    Added nuget package for bluetooth

--- Enigma/Enigma/Enigma.Android/BlueToothManager.cs
+++ Enigma/Enigma/Enigma.Android/BlueToothManager.cs
@@ -8,14 +8,23 @@ using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 using Enigma.BlueTooth.Mock;
+using MvvmCross.Platform;
+using Plugin.BLE.Abstractions.Contracts;
 using Xamarin.Forms;
+using IAdapter = Android.Widget.IAdapter;
 
 [assembly: Dependency(typeof(Enigma.Droid.BlueToothManager))]
 namespace Enigma.Droid
 {
     public class BlueToothManager : Enigma.BlueTooth.Mock.BlueToothManager
     {
+        public void initializeBluetooth()
+        {
+            var ble = Mvx.Resolve<IBluetoothLE>();
+            var adapter = Mvx.Resolve<IAdapter>();
+        }
+
+
     }
 }
---
+++ Enigma/Enigma/Enigma.Android/Bootstrap/BlePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Enigma.Droid.Bootstrap
+{
+    public class BlePluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.BLE.PluginLoader>
+    {
+    }
+}
