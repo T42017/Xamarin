89a5625b62cf6971a60efa68aa578fb404c60aec
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-12-14 10:50:09 +0100

    added Newton.json and JsonClass

---
+++ Enigma/Enigma/Enigma/JsonClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma
+{
+    public class JsonClass
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double posx { get; set; }
+        public double posy { get; set; }
+    }
+}
