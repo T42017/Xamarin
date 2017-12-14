fda18dee576d0f26b6f962b475d361887a98dcb2
JohanWikstrom <johan.wikstrom@lund.ntig.se>
2017-12-14 12:01:53 +0100

    Manually merged in forms_maps

--- Enigma/Enigma/Enigma/MapPage.xaml.cs
+++ Enigma/Enigma/Enigma/MapPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Maps;
+using Xamarin.Forms.GoogleMaps;
 
 namespace Enigma
 {
@@ -13,41 +13,30 @@ namespace Enigma
 		public MapPage ()
 		{
 		    InitializeComponent();
-		    var map = new Map(
-		        MapSpan.FromCenterAndRadius(
-		            new Position(55.686047, 13.185772), Distance.FromMiles(1.0)))
+		    var map = new Map()
 		    {
-		        IsShowingUser = true,
+                MyLocationEnabled = true,
+                		        
 		        HeightRequest = 320,
 		        WidthRequest = 200,
 		        VerticalOptions = LayoutOptions.CenterAndExpand
 		    };
-		    var position = new Position(55.686047, 13.185772);
-		    var pin = new Pin
+            
+		    var position = new Position(55.696922, 13.195254);
+		    var pin = new Pin()
 		    {
+                
 		        Type = PinType.Place,
 		        Position = position,
-		        Label = "NTI-Gymnasiet",
-		        Address = "Sankt Lars väg 11, 222 70 Lund"
-            };
-
-		    var position2 = new Position(55.376692, 13.145149);
-		    var pin2 = new Pin
-		    {
-		        Type = PinType.Place,
-		        Position = position2,
-		        Label = "Trelleborgen",
-		        Address = "Västra Vallgatan 6, 231 64 Trelleborg"
-            };
-
-            map.Pins.Add(pin);
-		    map.Pins.Add(pin2);
-            //var cp = new ContentPage
-            //{
-            //    Content = map,
-            //};
-
-            this.Content = map;
+                Icon = BitmapDescriptorFactory.FromBundle("meme.png"),
+		        Label = "custom pin",               
+		        Address = "custom detail info"
+		    };
+		    map.Pins.Add(pin);
+		    map.UiSettings.CompassEnabled = true;
+		    var position1= new Position(56.172442, 14.860493);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(position1, Distance.FromKilometers(150)));
+		    this.Content = map;
         }
 	}
 }
