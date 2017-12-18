18c7275bd314ba737af68ef6e49752ca032df764
Mathias Andersson <mathias.andersson@lund.ntig.se>
2017-12-18 10:10:14 +0100

    Splash Screen fits my phone and the good emulator. Added background to connect page.

--- Enigma/Enigma/Enigma.Android/Resources/Resource.Designer.cs
+++ Enigma/Enigma/Enigma.Android/Resources/Resource.Designer.cs
@@ -2330,26 +2330,26 @@ namespace Enigma.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_1 = 2130837819;
-			
 			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_2 = 2130837820;
+			public const int avd_hide_password_1 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_3 = 2130837821;
+			public const int avd_hide_password_2 = 2130837821;
+			
+			// aapt resource value: 0x7f02013e
+			public const int avd_hide_password_3 = 2130837822;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_1 = 2130837822;
-			
 			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_2 = 2130837823;
+			public const int avd_show_password_1 = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int avd_show_password_3 = 2130837824;
+			public const int avd_show_password_2 = 2130837824;
+			
+			// aapt resource value: 0x7f020141
+			public const int avd_show_password_3 = 2130837825;
 			
 			// aapt resource value: 0x7f020056
 			public const int Bluetooth = 2130837590;
@@ -3011,11 +3011,11 @@ namespace Enigma.Droid
 			// aapt resource value: 0x7f020131
 			public const int notification_icon_background = 2130837809;
 			
-			// aapt resource value: 0x7f020139
-			public const int notification_template_icon_bg = 2130837817;
-			
 			// aapt resource value: 0x7f02013a
-			public const int notification_template_icon_low_bg = 2130837818;
+			public const int notification_template_icon_bg = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int notification_template_icon_low_bg = 2130837819;
 			
 			// aapt resource value: 0x7f020132
 			public const int notification_tile_bg = 2130837810;
@@ -3037,6 +3037,9 @@ namespace Enigma.Droid
 			
 			// aapt resource value: 0x7f020138
 			public const int splashscreen = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int splashscreendark = 2130837817;
 			
 			static Drawable()
 			{
